dceff0795505eab22aaf4289e195c97566bd5bc9
Cameron L <42598233+levezuc@users.noreply.github.com>
2019-09-27 21:30:16 -0600

    Enemy update
    added more enemy functionality

--- My Little Robot Heroes!/assets/CL_DEV_Folder/Enemy.cs
+++ My Little Robot Heroes!/assets/CL_DEV_Folder/Enemy.cs
@@ -19,11 +19,6 @@ public class Enemy : MonoBehaviour
 
     public GameObject CurrentNode;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //target = WayPoints.points[0];
-    }
 
     // Update is called once per frame
     void Update()
@@ -40,7 +35,7 @@ public class Enemy : MonoBehaviour
     {
         Vector3 nextPos = this.transform.position - CurrentNode.transform.position;
         transform.position -= nextPos.normalized * Time.deltaTime * Speed;
-        if(nextPos.magnitude < 0.5)
+        if (nextPos.magnitude < 0.5)
         {
             this.CurrentNode = CurrentNode.GetComponent<Path>().NextNode;
         }
@@ -54,21 +49,35 @@ public class Enemy : MonoBehaviour
     void takeDamage(int damage)
     {
         Health -= damage;
-        if (Health <= 0) {
+        if (Health <= 0)
+        {
             Die();
         }
     }
 
     void Die()
     {
-        //add whatever charge to the charge container 
-        //GameObject go = GameObject.Find()
+        GameObject core = GameObject.Find("Core");
+        GameControl gc = core.GetComponent<GameControl>();
+        gc.IncreaseCharge((int)Charge);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("hey gotem");
+        if (collision.gameObject.tag == "Core")
+        {
+            Debug.Log("hey gotem");
+            dealDamage();
+            Destroy(this.gameObject);
+        }
     }
 
     void dealDamage()
     {
-        GameObject core = GameObject.FindGameObjectWithTag("core");
-        //core.Ge
+        GameObject core = GameObject.FindGameObjectWithTag("Core");
+        GameControl gc = core.GetComponent<GameControl>();
+        gc.DecreaseHealth((int)this.Damage);
     }
 
 
