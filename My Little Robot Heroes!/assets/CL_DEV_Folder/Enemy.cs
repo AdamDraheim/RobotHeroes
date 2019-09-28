2a32a48204147abae61a100f91c69d2024b775a7
Ethan Bowen <Ethankbowen@gmail.com>
2019-09-27 21:48:55 -0600

    Bullets delete themselves

--- My Little Robot Heroes!/assets/CL_DEV_Folder/Enemy.cs
+++ My Little Robot Heroes!/assets/CL_DEV_Folder/Enemy.cs
@@ -17,7 +17,7 @@ public class Enemy : MonoBehaviour
 
     private int nextUpdate = 1;
 
-    public GameObject CurrentNode;
+    public GameObject CurrentNode = new GameObject();
 
 
     // Update is called once per frame
