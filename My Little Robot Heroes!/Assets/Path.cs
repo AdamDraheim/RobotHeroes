c2d0f74188dd468b01d1ca827e53dfcf8ad6a0e2
Sam Henderson <u1263910@ad.utah.edu>
2019-09-27 19:26:42 -0600

    Made nodes

---
+++ My Little Robot Heroes!/Assets/Path.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public GameObject NextNode;
+    public bool isStartNode;
+    public GameObject GetCurrentNode()
+    {
+        return this.gameObject;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
