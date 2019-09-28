430539e836445648880c5748c89043ab2acd4cec
Jane Tian <u1295169@ad.utah.edu>
2019-09-27 19:12:31 -0600

    create and drag new gameObject on click

---
+++ My Little Robot Heroes!/Assets/Scripts/TowerSpawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSpawn : MonoBehaviour
+{
+    public GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        place();
+    }
+
+    void place()
+    {
+        Vector3 pos = Input.mousePosition;
+        pos = Camera.main.ScreenToWorldPoint(pos);
+        pos += new Vector3(0, 0, 10);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(obj, pos, Quaternion.identity);
+        }
+
+    }
+
+}
