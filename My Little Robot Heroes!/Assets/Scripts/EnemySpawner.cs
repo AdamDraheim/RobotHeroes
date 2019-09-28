18062ff9579c465e02966aa8968e94c26d949d97
Daniel Jin Lok Ng <dndanielng929@gmail.com>
2019-09-27 20:12:50 -0600

    GameManager Singleton for wave management and enemy spawning

--- My Little Robot Heroes!/Assets/Scripts/EnemySpawner.cs
+++ My Little Robot Heroes!/Assets/Scripts/EnemySpawner.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;//prefab of enemy that this spawner spawns
+
     public float timer;//timer keeps track of time passed
+
     public float spawnTime;//time between each enemy spawn
+    public float spawnerLevel;//high level spawners spawn enemies faster OR has a longer wave
+    public float enemiesForCurrentWave;
 
     // Start is called before the first frame update
     void Start()
     {
         timer = 0f;
         spawnTime = 2f;
+        enemiesForCurrentWave = 10 + GameManager.instance.waveNumber * 3; //algorithm to dictate how many enemies spawn for each increasing wave number
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer > spawnTime)
+        if (enemiesForCurrentWave > 0)
         {
-            timer = 0f;
-            SpawnEnemy();
+            if (timer > spawnTime)
+            {
+                timer = 0f;
+                SpawnEnemy();
+            }
+        }
+        else
+        {
+            //current wave is over, destroys the spawner
+            GameManager.instance.waveNumber++;
+            GameManager.instance.startNextWave = true;
+            Destroy(gameObject);
         }
         timer += Time.deltaTime;
     }
@@ -29,6 +44,7 @@ public class EnemySpawner : MonoBehaviour
     public void SpawnEnemy()
     {
         Instantiate(enemyPrefab);
+        enemiesForCurrentWave--;
         Debug.Log("spawn enemy");
     }
 }
---
+++ My Little Robot Heroes!/Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { private set; get; }//Singleton
+
+    public GameObject enemySpawnerPrefab;
+    public GameObject spawnLocation;//empty game object placed at location of enemy spawns
+    public Transform spawnLocationTransform;
+
+    [Header("Waves")]
+    public int waveNumber;
+    public bool startNextWave;//true if next wave timer should start
+    public float timer;
+    public float waveTime;//time until next wave
+
+    private void Awake()
+    {
+        //Singleton
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //init values
+        waveNumber = 1;
+        spawnLocationTransform = spawnLocation.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > waveTime && startNextWave == true)
+        {
+            timer = 0f;
+            StartNextWave();
+        }
+        timer += Time.deltaTime;
+    }
+
+    /// <summary>
+    /// Starts next wave by instantiating a new enemy spawner
+    /// </summary>
+    public void StartNextWave()
+    {
+        startNextWave = false;
+        Instantiate(enemySpawnerPrefab, spawnLocationTransform);
+    }
+}
